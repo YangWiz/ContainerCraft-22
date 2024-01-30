3b6b9cc2383969e1d91a031b7217b9f13dbcd4fb
YangHoo! <PolymagicYang@protonmail.com>
2024-01-30 16:36:33 +0100

    fix db connection bugs.

--- backend/database.py
+++ backend/database.py
@@ -5,16 +5,11 @@ from models import Base, Item
 class db:
     def __init__(self, db_url: str):
         self.offline = False
-        try:
-            self.engine = create_engine(db_url)
-            self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind = self.engine)
-            Base.metadata.create_all(bind = self.engine)
-        except:
-            self.offline = True
+        self.engine = create_engine(db_url)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind = self.engine)
+        Base.metadata.create_all(bind = self.engine)
 
     def connect(self):
-        if self.offline:
-            return None
         db = self.SessionLocal()
         try:
             return db
